67d033e34add71fc1bca8831bb02bf5dce6a5a32
Joseph Hunter <jthntr@Josephs-MacBook-Air.local>
2021-04-24 19:45:24 -0600

    adding main.cpp

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void displayMemu();
+
+int main()
+{
+    int userInput = 0;
+
+    while(userInput != 5)
+    {
+       switch(userInput)
+       {
+           case 1:
+           {
+
+           }
+
+           case 2:
+           {
+
+           }
+
+           case 3:
+           {
+
+           }
+
+           case 4:
+            {
+
+            }
+
+            case 5:
+            {
+                
+            }
+       } 
+    }
+
+}
+
+void displayMenu()
+{
+    cout << "Select a numerical option:" << endl;
+    cout << "+=====Main Menu=====+" << endl;
+    cout << "1. " << endl;
+    cout << "2. " << endl;
+    cout << "3. " << endl;
+    cout << "4. " << endl;
+    cout << "5. Quit " << endl;
+    cout << "+===================+" << endl;
+}
